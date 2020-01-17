fa8fb6437735837140989a715ccea02060bda57c
YudjinSud <jeka.sudakov@mail.ru>
2020-01-17 15:54:28 +0300

    denis weapper 2

--- packages/id_read/scripts/id_read.py
+++ packages/id_read/scripts/id_read.py
@@ -8,15 +8,17 @@ from std_msgs.msg import String
 pub = rospy.Publisher("/topic_readed_id", AprilTagsWithInfos, queue_size = 10)
 
 def handle_msg(msg):
-	for i in range(10):
-		print("ID DETECTED!!!!!!")
+	pub.publish(msg)
+	pass
+	#for i in range(10):
+	#	print("ID DETECTED!!!!!!")
     	#pub.publish(msg)
-	for i in msg.detections:
+	#for i in msg.detections:
 		#pub.publish(i.id) 
-		for j in range(5):
-			print("ID DETECTED :: :: " + str(i.id))
+		#for j in range(5):
+			#print("ID DETECTED :: :: " + str(i.id))
 if __name__ == '__main__':
   rospy.init_node('id_reader')
-  sub = rospy.Subscriber('/autobot02/apriltags_postprocessing_node/apriltags_out', AprilTagsWithInfos,handle_msg,queue_size=10)
+  sub = rospy.Subscriber('/autobot02/apriltags_postprocessing_node/apriltags_out', AprilTagsWithInfos, handle_msg,queue_size=10)
   print("Hello wordl")
   rospy.spin()
