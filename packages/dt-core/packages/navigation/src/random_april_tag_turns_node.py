a1e321c9555eefddfb636f0ef8eeee42d56dd0f6
YudjinSud <jeka.sudakov@mail.ru>
2020-01-17 13:32:17 +0300

    rand turns eewfsfr

--- packages/dt-core/packages/navigation/src/random_april_tag_turns_node.py
+++ packages/dt-core/packages/navigation/src/random_april_tag_turns_node.py
@@ -77,7 +77,7 @@ class RandomAprilTagTurnsNode(object):
                     #now randomly choose a possible direction
                 if(len(availableTurns)>0):
                     randomIndex = numpy.random.randint(len(availableTurns))
-                    chosenTurn = availableTurns[randomIndex]
+                    chosenTurn = 1
                     self.turn_type = chosenTurn
                     self.pub_turn_type.publish(self.turn_type)
 
